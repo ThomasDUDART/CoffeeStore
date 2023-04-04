53b8a5f44d7151a7d66215ba71c57335c5250645
Thomas <thomas.dudart2@etu.univ-lorraine.fr>
2023-04-04 10:39:50 +0200

    Commit Vide

--- src/main/java/com/example/coffeestore/controller/EmployeeController.java
+++ src/main/java/com/example/coffeestore/controller/EmployeeController.java
@@ -36,7 +36,6 @@ public class EmployeeController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "Vitrine";
-
     }
 
     @GetMapping("/Maison")
